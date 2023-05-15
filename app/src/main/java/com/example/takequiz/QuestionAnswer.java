1bfd35eadce3e99facc074550966eff43476f9bf
Anoop <anooptiwari597@gmail.com>
2023-05-15 12:58:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/takequiz/QuestionAnswer.java
@@ -0,0 +1,46 @@
+package com.example.takequiz;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Which one is not the programming language ?",
+            "Who is current Prime Minister of India ?",
+            "Which of the following has a global scope in the program?",
+            "What is the national bird of India ?",
+            "Which company owns the android ?",
+            "Which is the mother of all programming languages ?",
+            "What is the capital of France?",
+            "Which planet is known as the Red Planet?",
+            "Which country is known as the Land of the Rising Sun?",
+            "Who discovered the theory of relativity?"
+    };
+
+    public static String choices[][]={
+            {"Java","Kotlin","Notepad","Python"},
+            {"Rahul Gandhi","Narendra Modi","Akhilesh Yadav","Yogi Aditynath"},
+            {"Formal Parameters","Constants","Macros","Local Variable"},
+            {"Crow","Ostrich","Peacock","Owl"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"C++","C","Python","Java"},
+            {" Berlin","Madrid","Paris","London"},
+            {"Venus","Mars","Saturn","Jupiter"},
+            {"Japan","Korea","Bangladesh","China"},
+            {" Isaac Newton","Albert Einstein","Galileo Galilei","Stephen Hawking"}
+
+
+
+    };
+
+    public static String correctAnswers[]={
+            "Notepad",
+            "Narendra Modi",
+            "Macros",
+            "Peacock",
+            "Google",
+            "C",
+            "Paris",
+            "Mars",
+            "Japan",
+            "Albert Einstein"
+    };
+
+}
